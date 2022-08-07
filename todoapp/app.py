b739af4fc431fef75701c91af422964691cc6474
Samuel Kow Paintsil <smashbros54@gmail.com>
2022-08-07 20:19:18 +0000

    ⚡️ zaaaap

--- todoapp/app.py
+++ todoapp/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
+# another streak
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://smashbros54:Meanboy6464@localhost:5432/udacityTodo'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False

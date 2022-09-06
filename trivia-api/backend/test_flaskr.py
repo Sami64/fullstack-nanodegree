795b994918dd987a412d5816bbff17fb213b8350
Samuel Kow Paintsil <smashbros54@gmail.com>
2022-09-06 09:26:09 +0000

    :sparkles: remove node_modules

--- trivia-api/backend/test_flaskr.py
+++ trivia-api/backend/test_flaskr.py
@@ -31,7 +31,7 @@ class TriviaTestCase(unittest.TestCase):
 
     """
     TODO
-    Write at least one test for each test for successful operation and for expected errors.
+    Write at least one test for each test for successfull operation and for expected errors.
     """
 
     def test_get_categories(self):
